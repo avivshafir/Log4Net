255daa79b49cb4095825dffe300b1eea54bb8dd6
ashafir <aviv@optimalfinance.co.il>
2014-10-28 21:57:17 +0200

    Added file appender for a simple gui application

---
+++ Log4Net.GuiAndFileAppender/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Log4Net.GuiAndFileAppender
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            var log = log4net.LogManager.GetLogger(typeof(Program));
+
+            log.Debug("Debug");
+            log.Info("Info");
+            log.Warn("Warn");
+            log.Error("Error");
+            log.Fatal("Fatal");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
