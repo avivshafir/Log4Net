2d7dcc826313b59afeafa3f3e8b60f17f28f3544
ashafir <aviv@optimalfinance.co.il>
2014-10-28 20:37:24 +0200

    Init. Added simple log demo console application

---
+++ Log4NetQuickStartDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Log4NetQuickStartDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+
+            var log = log4net.LogManager.GetLogger(typeof (Program));
+
+            log.Info("Hello from log4net!");
+
+            Console.ReadLine();
+        }
+    }
+}
